8b2f4b963845097a27a2e0302890ef834515162d
hongwuzhao <hongwuzhao@tencent.com>
2018-12-21 14:55:01 +0800

    增加设计模式原则

---
+++ code_demo/可变长文案/可变长文案/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  可变长文案
+//
+//  Created by hongwuzhao on 12/17/18.
+//  Copyright © 2018 tencent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
